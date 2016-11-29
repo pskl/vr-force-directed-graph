0ba893d6a703a80d8eab8747d7a16ed43b616222
lbeisel <lbeisel@mpiwg-berlin.mpg.de>
2016-11-29 19:12:26 +0100

    Fixed Scripts

---
+++ VR Force Directed Graph/Assets/Scripts/Graph.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Graph : MonoBehaviour
+{
+
+
+	public int repulsion;
+	public int attraction;
+	public int springLength;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
