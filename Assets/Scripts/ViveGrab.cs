2cd2c423b476b2737b809509e088e2d6097acd08
JonChray <ycx@live.de>
2016-11-30 12:29:42 +0100

    grab1

--- Assets/Scripts/ViveGrab.cs
+++ Assets/Scripts/ViveGrab.cs
@@ -2,14 +2,52 @@
 using System.Collections;
 
 public class ViveGrab : MonoBehaviour {
-    
-	void Start ()
+
+    GameObject grabbedObj;
+
+    Node colNode;
+
+    SteamVR_TrackedObject trackedObj;
+    FixedJoint joint;
+
+    void Awake()
     {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
 	
-	}
 	
 	void Update ()
     {
-	
-	}
+        if (colNode != null)
+        {
+            print("setPosition");
+            colNode.transform.position = transform.position;
+        }
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            print("trigegerUp");
+            colNode = null;
+        }
+    }
+
+    private void OnTriggerStay(Collider c)
+    {
+
+        print("triggerIsEneter");
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            print("triggerDown");
+            colNode = c.GetComponent<Node>();
+        }
+    }
+
+    
 }
