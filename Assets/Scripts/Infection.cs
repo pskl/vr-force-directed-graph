baf0e11295ce783b67da80dd04530d7b9a875411
JonChray <ycx@live.de>
2016-12-01 14:20:30 +0100

    gameplay yo

--- Assets/Scripts/Infection.cs
+++ Assets/Scripts/Infection.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Infection : MonoBehaviour {
 
@@ -13,10 +15,19 @@ public class Infection : MonoBehaviour {
     {
         if(Input.GetKeyDown(KeyCode.Return))
         {
-
-            int infectedID = Random.Range(0, graph.nodes.Count-1);
-            graph.nodes[infectedID].FixedInfection();
+            SpreadInfection();
         }
+    }
+
+
+    public void SpreadInfection()
+    {
+        graph.nodes = graph.nodes.OrderBy(x => x.attractionlist.Count).ToList();
+        Node firstnode = graph.nodes.First();
+
+        List<Node> possibleNodes = graph.nodes.FindAll(x => x.attractionlist.Count == firstnode.attractionlist.Count).ToList();
+
+        possibleNodes[Random.Range(0, possibleNodes.Count - 1)].FixedInfection();
     }
     
 }
